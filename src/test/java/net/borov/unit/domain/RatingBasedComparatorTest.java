3b4d9967aa71bcea05fd29ebe597d98bf2cf911f
WonderBeat <borov.htid@gmail.com>
2013-08-21 21:57:19 +0400

    Rated element domain object
    +RatingBasedComparator

---
+++ src/test/java/net/borov/unit/domain/RatingBasedComparatorTest.java
@@ -0,0 +1,16 @@
+package net.borov.unit.domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RatingBasedComparatorTest {
+    @Test
+    public void shouldCompareElements() throws Exception {
+        RatedElement element = new RatedElement("ONE", 2);
+        RatedElement greaterElement= new RatedElement("TWO", 3);
+        RatingBasedComparator comparator = new RatingBasedComparator();
+        Assert.assertEquals(-1, comparator.compare(element, greaterElement));
+        Assert.assertEquals(1, comparator.compare(greaterElement, element));
+        Assert.assertEquals(0, comparator.compare(element, element));
+    }
+}
